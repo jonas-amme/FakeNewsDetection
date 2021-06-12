bd3cab244293e22b33493183b19abedebcf18a34
jonas-amme <79060687+jonas-amme@users.noreply.github.com>
2021-06-12 16:41:32 +0200

    implemented cross validation

--- train.py
+++ train.py
@@ -1,16 +1,10 @@
-
 import time
-from tqdm import tqdm
 import argparse
 import random
+from sklearn.metrics import f1_score, accuracy_score, recall_score, precision_score
 
-import torch
-import torch.nn.functional as F
-from torch_geometric.data import DataLoader
-from sklearn.metrics import f1_score, accuracy_score, recall_score, precision_score, roc_auc_score
-
-from LoadData import LoadData
-from preprocess import normalizeFeatures
+from LoadData import *
+from preprocess import *
 from models import *
 
 
