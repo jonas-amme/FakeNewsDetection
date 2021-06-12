a70963d67e8dd035e5a8b1549bb04c3d6a3ed930
jonas-amme <79060687+jonas-amme@users.noreply.github.com>
2021-06-12 17:14:08 +0200

    remove empty cascades

--- LoadData.py
+++ LoadData.py
@@ -2,13 +2,9 @@
 LoadData class for preparing news cascades for model training
 """
 
-# load libraries
 import os
-import numpy as np
 from tqdm import tqdm
 import torch
-from torch_geometric.data import DataLoader
-from torch.utils.data import random_split
 
 
 # create data loader class object
@@ -23,21 +19,16 @@ class LoadData:
     def _get_data_objects(self):
         fake = 0
         real = 0
-        # TODO: add tqdm and print statements
         print('====== Start collecting cascades ======')
         for file in tqdm(self.files):
             filename = os.path.join(self.data_folder, file)
             data_obj = torch.load(filename)
             if len(data_obj.x) == 0: # remove empty cascades
                 continue
-            if data_obj.y == 1: # TODO: remove constraints to load full data
-                # if real == 1000:
-                #     continue
+            if data_obj.y == 1:
                 self.real_data.append(data_obj)
                 real += 1
-            else: # TODO: remove constraints to load full data
-                # if fake == 1000:
-                #     continue
+            else:
                 self.fake_data.append(data_obj)
                 fake += 1
             self.graph_data.append(data_obj)
